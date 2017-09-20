86c54c447bbec0586256c0ec02869b6bffbcc6a6
Jessica Song <jpark2973@gmail.com>
2017-09-20 14:07:55 -0700

    stateless Functional components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react'
 
-class ListContacts extends Component {
-  render (){
-    console.log(  this.props )
-    return (
-      <ol className='contact-list'>
-      {this.props.contacts.map( ele => (
-        <li key={ele.name} className='contact-list-item'>
-          <div className='contact-avatar' style={{backgroundImage: `url(${ele.avatarURL})`}}/>
-          <div className='contact-details'>
-            <p>{ele.name}</p>
-            <p>{ele.email}</p>
-          </div>
-          <button className='contact-remove'>Remove</button>
+function ListContacts (props) {
+  return (
+    <ol className='contact-list'>
+    {props.contacts.map( ele => (
+      <li key={ele.name} className='contact-list-item'>
+        <div className='contact-avatar' style={{backgroundImage: `url(${ele.avatarURL})`}}/>
+        <div className='contact-details'>
+          <p>{ele.name}</p>
+          <p>{ele.email}</p>
+        </div>
+        <button className='contact-remove'>Remove</button>
 
-        </li>
-      ))}
-      </ol>
-    )
-  }
+      </li>
+    ))}
+  </ol>
+  )
 }
 
 
